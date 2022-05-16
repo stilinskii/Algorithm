b79ef56f3caa47e4aa04ccaa275e29d9d936f41f
stilinskii <lhy1031hy@gmail.com>
2022-05-17 01:52:07 +0900

    Binary Search

---
+++ src/Algorithm_Basic/SearchAlgorithm.java
@@ -0,0 +1,41 @@
+package Algorithm_Basic;
+
+//[?] 정렬되어있는 데이터를 이진검색(이분탐색)을 사용하여 반씩 나눠서 검색
+
+public class SearchAlgorithm {
+    public static void main(String[] args) {
+        //input
+        int[] data ={1,3,5,7,9};//오름차순으로 정렬되었다고 가정
+        int N = data.length;
+        int search = 7;
+        boolean flag = false; // 찾았으면 true 그렇지않으면 false
+        int index = -1; //찾은위치
+        //process (Binary Search)
+        int low = 0;
+        int last = N-1;
+        int mid = (low+last)/2;
+
+        while(true){
+            if(data[mid]==search){
+                flag = true;
+                index = mid;
+                break;
+            }else if(data[mid]<search){
+                low = mid +1;
+                mid = (low+last)/2;
+
+            }else if(data[mid]>search){
+                last = mid-1;
+                mid = (low+last)/2;
+            }
+        }
+
+
+        //output
+        if(flag){
+        System.out.println(search+"는"+index+"번째 열에 있습니다");
+        }else{
+            throw new IllegalStateException("찾지못했습니다.");
+        }
+    }
+}
